74329cdbd917ec88ab7ebe9c4e5b95028ba1addf
Joyce Ledi VI <joyceledi26@gmail.com>
2025-01-17 13:51:52 +0100

    Added Message,Shared

---
+++ Shared/HRManagement.Message.Shared/UserRegisterMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace HRManagement.Message.Shared
+{
+    public class UserRegisterMessage
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime RegistrationDate { get; set; } = DateTime.Now;
+    }
+}
